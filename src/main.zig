d2508f91cd06c35e659516d3b507e89e911bf30a
Krieg <69888212+blitz-krieg-cmd@users.noreply.github.com>
2024-09-03 13:38:31 -0600

    working on dcx

--- src/main.zig
+++ src/main.zig
@@ -1,3 +1,7 @@
 const std = @import("std");
+const dcx = @import("dcx.zig");
 
-pub fn main() !void {}
+pub fn main() !void {
+    std.debug.print("{any}\n", .{try dcx.is("E:\\SteamLibrary\\steamapps\\common\\DARK SOULS REMASTERED\\param\\GameParam\\GameParam.parambnd.dcx")});
+    std.debug.print("{s}\n", .{try dcx.decompress("E:\\SteamLibrary\\steamapps\\common\\DARK SOULS REMASTERED\\param\\GameParam\\GameParam.parambnd.dcx")});
+}
