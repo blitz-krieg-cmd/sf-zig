e5435d5572e26920b519fbe837fad43f5b3e0332
Krieg <69888212+blitz-krieg-cmd@users.noreply.github.com>
2024-09-10 16:34:03 -0600

    reworked main for args and error checking

--- src/main.zig
+++ src/main.zig
@@ -2,21 +2,42 @@ const std = @import("std");
 const formats = @import("sf.zig");
 
 pub fn main() !void {
-    // allocator
     var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
     defer arena.deinit();
     const allocator = arena.allocator();
 
-    // file work
-    var file = try std.fs.openFileAbsolute("E:/SteamLibrary/steamapps/common/DARK SOULS REMASTERED/param/GameParam/GameParam.parambnd.dcx", .{ .mode = .read_only });
+    const argv = std.process.argsAlloc(allocator) catch |err| {
+        std.log.err("Error: {?}", .{err});
+        return;
+    };
+    defer std.process.argsFree(allocator, argv);
+
+    if (argv.len == 1) {
+        std.log.info("Usage: {s} <filename> <options>\n", .{argv[0]});
+        return;
+    }
+
+    const filePath = argv[1];
+    var file = std.fs.openFileAbsolute(
+        filePath,
+        .{ .mode = .read_only },
+    ) catch |e| switch (e) {
+        error.FileNotFound => {
+            std.log.err("File '{s}' does not exist\n", .{filePath});
+            return;
+        },
+        else => {
+            std.log.err("{?}\n", .{e});
+            return;
+        },
+    };
     defer file.close();
 
-    // file into buffer
     const fileBytes = try file.readToEndAlloc(allocator, try file.getEndPos());
     defer allocator.free(fileBytes);
 
     const dcx = try formats.ReadDCX(fileBytes, allocator);
     const bnd = try formats.ReadBND3(dcx.uncompressedBytes, allocator);
 
-    std.debug.print("{c}\n", .{bnd.header.magic});
+    std.log.info("{c}\n", .{bnd.header.magic});
 }
