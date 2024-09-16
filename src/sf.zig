3ff84ea35312a483cd75ee7db6ac590d96262eb8
Krieg <69888212+blitz-krieg-cmd@users.noreply.github.com>
2024-09-15 23:24:40 -0600

    Update sf.zig

--- src/sf.zig
+++ src/sf.zig
@@ -1,5 +1,3 @@
 // Formats
 pub const ReadDCX = @import("formats/dcx.zig").read;
-pub const ReadBND3 = @import("formats/bnd.zig").read;
-// pub const PARAM = @import("formats/param.zig").PARAM;
-// pub const PARAMDEF = @import("formats/param.zig").PARAMDEF;
+pub const ReadBND3 = @import("formats/bnd.zig").read2;
